63efe9c13b6802891d34f16306ddb14ca239635c
smaaliyan <smaaliyan0@gmail.com>
2023-08-16 11:28:37 +0500

    make validation code works

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,83 +1,65 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\todo;
 use Illuminate\Validation\ValidationException;
-    
+
 use Illuminate\Http\Request;
 
 
 class TodoController extends Controller
 {
-    public function index(){
+    public function index()
+    {
 
         $todos = Todo::all();
         return view('index')->with('todos', $todos);
-    
     }
-    public function create(){
+    public function create()
+    {
         return view('create');
     }
-    public function details(Todo $todo){
+    public function details(Todo $todo)
+    {
 
         return view('details')->with('todos', $todo);
-    
     }
-    
-    public function edit(){
-    
+
+    public function edit()
+    {
+
         return view('edit');
-    
     }
-    public function update(){
-    
+    public function update()
+    {
+
         //we will write codes for updating a todo here
-    
+
     }
-    public function delete(){
-    
+    public function delete()
+    {
+
         //we will write codes for deleting a todo here
-    
+
     }
 
-    public function store(){
+    public function store(Request $request)
+    {
 
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required'
+        ]);
 
-        try {
-            $this->validate(request(), [
-                'name' => 'required',
-                'description' => 'required'
-            ]);
-        } catch (ValidationException $e) {
-            dd('Validation failed:', $e->getMessage());
-        }
-
-
-        $name = request()->input('name');
-        $description = request()->input('description');
-        dd(request()->all());
-
-        // Make sure the array is not null before accessing its elements
-        if (isset($data['name']) && isset($data['description'])) {
-            $todo = new Todo();
+        $todo = new Todo();
 
         $todo = new todo();
         //On the left is the field name in DB and on the right is field name in Form/view
-        $todo->name = $name['name'];
-        $todo->description = $description['description'];
+        $todo->name = $request['name'];
+        $todo->description = $request['description'];
         $todo->save();
         session()->flash('success', 'Todo created succesfully');
         return redirect('/');
-        
-
     }
-
-
-
-
-
-
-
-
-}
 }
--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -11,10 +11,17 @@
         <div class="form-group m-3">
             <label for="name">Todo Name</label>
             <input type="text" class="form-control" name="name">
+            @error('name')
+            <div class="text-red-500">{{ $message }}</div>
+        @enderror
+    
         </div>
         <div class="form-group m-3">
             <label for="description">Todo Description</label>
             <textarea class="form-control" name="description" rows="3"></textarea>
+            @error('description')
+            <div class="text-red-500">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group m-3">
             <input type="submit" class="btn btn-primary float-end" value="Submit">
